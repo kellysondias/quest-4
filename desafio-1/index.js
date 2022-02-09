7d45a1b886c9a1abc403e2c7502f044448e17212
Kellyson Dias <kellysondias@outlook.com>
2022-02-09 17:35:10 -0300

    First commit

---
+++ desafio-1/index.js
@@ -0,0 +1,47 @@
+let rightArrow = document.getElementById("seta-direita")
+
+let images = document.getElementsByClassName('img')
+
+rightArrow.addEventListener('click', () => {
+    images.forEach(() => {
+        images.classList.add('show') 
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let SetaDireita = document.getElementById("seta-direita")
+
+SetaDireita.addEventListener('click', () => {
+
+    let ImagemDeFundo = document.getElementsByTagName('body')
+    
+    ImagemDeFundo[0].style.backgroundImage = 'url(src/img/2.jpg)'
+    
+    let SetaEsquerda = document.getElementById("seta-esquerda")
+    
+    SetaEsquerda.innerHTML = `<img src="src/img/seta-esquerda.png" id="seta-esquerda" alt="Seta esquerda">`
+    
+    let Titulo = document.getElementsByTagName("h1")
+    
+    Titulo[0].innerHTML = `<h1>Titulo imagem 2</h1>`
+    
+    let Link = document.getElementById("link")
+    
+    Link.style.color = "#d9dd10"
+}) */

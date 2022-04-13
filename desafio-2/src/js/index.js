b543a798d6a1fb8196660552a309115159fdbde6
Kellyson <Kellysondias@outlook.com>
2022-04-13 11:27:32 -0300

    Adicionando um operador ternário no algoritmo

--- desafio-2/src/js/index.js
+++ desafio-2/src/js/index.js
@@ -1,10 +1,9 @@
 const caixaDeTexto = document.querySelectorAll('.caixa-de-texto')
 
-console.log(caixaDeTexto)
 caixaDeTexto.forEach(texto => {
     texto.addEventListener('click', () => {
-        console.log(texto)
-        texto.classList.remove('texto-ativo')
-        texto.classList.add('texto-ativo')
+        textoAtivo = document.querySelector
+        ('.texto-ativo')
+        textoAtivo ? texto.classList.remove('texto-ativo') : texto.classList.add('texto-ativo')
     })
 })

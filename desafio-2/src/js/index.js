e7692137a6aaba0b045697bea92ce31d80eb9793
Kellyson <Kellysondias@outlook.com>
2022-04-13 21:05:33 -0300

    Refatorando

--- desafio-2/src/js/index.js
+++ desafio-2/src/js/index.js
@@ -3,10 +3,7 @@ const perguntas = document.querySelectorAll('.caixa-de-texto')
 perguntas.forEach( pergunta => {
     pergunta.addEventListener('click', () => {
         const perguntaAtiva = document.querySelector('.texto-ativo')
-        
         pergunta.classList.add('texto-ativo')
-
         if (perguntaAtiva) perguntaAtiva.classList.remove('texto-ativo')
-
     })
 })

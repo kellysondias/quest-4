530fb934c5b7bfec3bcd4c046e413678cc5ca78a
Kellyson <Kellysondias@outlook.com>
2022-04-28 16:48:53 -0300

    Adicionando o event listener'

---
+++ desafio-3/src/js/index.js
@@ -0,0 +1,7 @@
+const inputs = document.getElementsByTagName('input')
+const textArea = document.getElementsByTagName('textarea')
+const button = document.getElementById('btn')
+
+button.addEventListener('click', () => {
+    console.log('xibiu')
+})

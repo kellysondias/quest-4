a05ac67bf894051882f2d6d69df139437dd45ba9
Kellyson <Kellysondias@outlook.com>
2022-05-04 20:46:12 -0300

    Testando event listener

--- desafio-3/src/js/index.js
+++ desafio-3/src/js/index.js
@@ -1,13 +1,22 @@
+const form = document.querySelector('form')
 const inputs = document.querySelectorAll('input')
 const textArea = document.querySelectorAll('textarea')
 const buttons = document.getElementById('btn')
 
-function addInputConditional() {
+/* function addInputConditional() {
     inputs.forEach(input => {
-        if (input.value == undefined) console.log('Condicional funcionando')
+        if (input.value === "") console.log('Condicional funcionando')
     })
-}
+} */
 
-buttons.addEventListener('click',() => {
-    addInputConditional()
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    this.forEach(element => {
+        if (element.value == "") console.log('Condicional funcionando')
+    })
 })
+
+/* buttons.addEventListener('click',() => {
+    addInputConditional()
+}) */

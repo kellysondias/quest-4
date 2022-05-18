6a5fb9f53af797fca35a53ccdd2998a61fbadf88
Kellyson <Kellysondias@outlook.com>
2022-05-17 21:42:27 -0300

    Editando o event listener 'keypress'

--- desafio-3/src/js/index.js
+++ desafio-3/src/js/index.js
@@ -1,25 +1,25 @@
-/* ToDo: - Validação dos formulários obrigatórios  
-
-    Refatoração: - Procurar input reset
-                 - Transformar event listener do button em função limpa
-*/
-
 const form = document.getElementById('form')
 const textInputs = document.querySelectorAll('.text-input')
 const errorMessages = document.querySelectorAll('p')
 
 form.addEventListener('submit', event => {
     event.preventDefault()
-    textInputs.forEach((textInput, index) => {
+    textInputs.forEach((textInput) => {
         if (textInput.value == '') {
             textInput.classList.add('error')
             textInput.nextElementSibling.classList.add('show')
+        } else {
+            textInput.classList.remove('error')
+            textInput.nextElementSibling.classList.remove('show')
         }
     })
 })
 
-textInputs.forEach(input => {
-    input.addEventListener('change', () => {
-        console.log('funcionou');
+textInputs.forEach(textInput => {
+    textInput.addEventListener('keypress', () => {
+        if (textInput.value !== '') {
+            textInput.classList.add('check')
+            textInput.nextElementSibling.classList.add('hide')
+        }
     })
 })
